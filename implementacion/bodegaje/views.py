bc1ba32d796efd75267d295bf47434d57f0b3c1e
Alison Daiana Aristizabal <a.aristizabalg@uniandes.edu.co>
2025-10-13 15:03:08 -0500

    Cambio Autenticación

--- implementacion/bodegaje/views.py
+++ implementacion/bodegaje/views.py
@@ -1,12 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 from .models import Producto, Ubicacion
 from .servicios import retirar_producto
 
 class RetiroView(APIView):
-    permission_classes = [IsAuthenticated]  # En dev puedes usar AllowAny
+    authentication_classes = []
+    permission_classes = [AllowAny]  # En dev puedes usar AllowAny
 
     def post(self, request):
         data = request.data
